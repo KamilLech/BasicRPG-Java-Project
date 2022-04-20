25586398f17fd3af3e60d942ba370336d3cf15d0
Kamil Lech <103774397+KamilLech@users.noreply.github.com>
2022-04-20 14:44:04 +0200

    podstawy okna rzutu koścmi - drobny fix (static)

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		DiceBagWindow window = new DiceBagWindow(900, 500, "RPG");
-		DiceBag diceBag = new DiceBag();
-		Dice k100 = diceBag.getK100();
-		int wynikRzutu;
-		wynikRzutu = k100.roll();
 		
 		
 	}
